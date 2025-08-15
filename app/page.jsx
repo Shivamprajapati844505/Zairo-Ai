b4ebd3bd3ad58d2a68f0af7ab6f6b75696cf358b
Shivamprajapati844505 <sp1172389@gmail.com>
2025-08-15 23:22:42 +0530

    still a work on sidebar

--- app/page.jsx
+++ app/page.jsx
@@ -15,8 +15,7 @@ export default function Home() {
        <Sidebar expand={expand} setExpand={setExpand}/>
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
-            <Image 
-            onClick={()=>(expand ? setExpand(false): setExpand(true))} className="rotate-180" src={assets.menu_icon} alt="" />
+            <Image onClick={()=>(expand ? setExpand(false): setExpand(true))} className="rotate-180" src={assets.menu_icon} alt="" />
             <Image className="opacity-70" src={assets.chat_icon} alt="" />
           </div>
 
