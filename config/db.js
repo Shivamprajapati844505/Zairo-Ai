bbb615b05cfc9141a8d6c78144a17ab9c3ecbd9d
Shivamprajapati844505 <sp1172389@gmail.com>
2025-08-19 14:12:08 +0530

    fixed mistake of DB import

--- config/db.js
+++ config/db.js
@@ -2,13 +2,20 @@ import mongoose from "mongoose";
 
 let cached = global.mongoose || {conn : null, promise: null};
 
-export default async function connectDb(){
+export default async function connectDB(){
     if(cached.conn) return cached.conn;
     if(!cached.promise){
-       cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose)=>mongoose);   
+       cached.promise = mongoose.connect(process.env.MONGODB_URI,{
+           dbName: "zairo",
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+            }
+       ).then((mongoose)=>mongoose);   
     }
     try {
         cached.conn = await cached.promise;
+        global.mongoose = cached;
+        console.log("MongoDB connected");
     } catch (error) {
         
         console.log("Error connecting to mogoDb",error);
