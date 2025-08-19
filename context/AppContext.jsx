18f310a45d11e817f745fcda1f9a1cf0ffb98cb6
Shivamprajapati844505 <sp1172389@gmail.com>
2025-08-20 00:00:11 +0530

    create context functions

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,6 +1,11 @@
 "use client";
-import { createContext, useContext } from "react";
-import { useUser } from "@clerk/nextjs";
+import { createContext, useContext, useState } from "react";
+import { useUser, useAuth } from "@clerk/nextjs";
+import { NextResponse } from 'next/server';
+import axios from "axios";
+import { toast } from "react-hot-toast"; 
+
+
 
 export const AppContext = createContext();
 
@@ -10,9 +15,64 @@ export const useAppContext = () => {
 
 export const AppContextProvider = ({ children }) => {
   const { user } = useUser();
+  const {getToken} = useAuth()
 
-  const value = {
+const [chats, setChats] = useState([]);
+const [selecetedChat, setSelecetedChat] = useState(null);
+
+
+const createNewChat = async ()=>{
+  try {
+    if(!user) return null;
+
+    const token = await getToken();
+
+    await axios.post('/api/chat/create',{},{headers:{
+      Authorization :`Bearer ${token}`
+    }})
+     fetchUserChats();
+  } catch (error) {
+    toast.error(error.message)
+  }
+}
+
+const fetchUserChats = async ()=>{
+  try {
+      const token = await getToken();
+
+     const {data} = await axios.get('/api/chat/get',{headers:{
+      Authorization :`Bearer ${token}`
+    }})
+    if(data.success){
+      console.log(data.data);
+      setChats(data.data)
+
+      if(data.data.length ===0){
+        await createNewChat();
+        return fetchUserChat();
+      }else{
+        data.data.sort((a,b)=> new Date(b.updatedAt) - new Date(a.updatedAt))
+      }
+    }else{
+      toast.error(data.message)
+    }
+  } catch (error) {
+    toast.error(error.message)
+  }
+}
+ussEffect(()=>{
+   if(user){
+    fetchUserChats(;)
+   }
+},[user]) 
+ 
+const value = {
     user,
+  chats,
+  selectedChat,
+  setSelectedChat,
+  createNewChat,
+  fetchUserChats,
   };
 
   return (
